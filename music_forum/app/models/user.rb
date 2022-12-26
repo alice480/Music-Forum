1ff79acbb6334024cce2396a612ec745323bf51c
Alice <sinyavina.alisa2003@mail.ru>
2022-12-26 16:55:06 +0300

    navbar and  links to reviews

--- music_forum/app/models/user.rb
+++ music_forum/app/models/user.rb
@@ -2,7 +2,7 @@
 
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeolsutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
